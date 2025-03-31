6d2ab821bd5a0f85c73f8c2eca99ee54924dc324
Kredo15 <kredo1511@yandex.ru>
2025-03-31 23:53:05 +0300

    продолжаем работу с bot

--- bot/bot_app/commands.py
+++ bot/bot_app/commands.py
@@ -4,9 +4,10 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram import F
 from aiogram.fsm.context import FSMContext
-
+from .app import bot
 from .data_fetcher import get_new_word
-from .keyboards import get_menu_inline, get_button_keyboard
+from .keyboards import get_menu_inline, get_button_keyboard,\
+                    get_category_inline, category
 from .state import WordsStudy
 
 
@@ -22,10 +23,26 @@ async def start_command(message: Message):
 
 
 @dp.callback_query(F.data == "new_word")
-async def send_word_study(callback: CallbackQuery, state: FSMContext):
-    res = await get_new_word(callback.message.from_user.id, 'популярные слова')
-    en_word = res[0].get('en_word').get('word')
-    ru_word = res[0].get('ru_word').get('word')
-    await callback.message.answer(f"{en_word} - {ru_word}", reply_markup=get_button_keyboard())
-    await state.set_state(WordsStudy.new_word)
+async def choose_category(callback: CallbackQuery, state: FSMContext):
+    await bot.answer_callback_query(callback.id, text='your text',
+                                    show_alert=True)
+    # await callback.message.answer('Выберите категорию', reply_markup=get_category_inline())
+    # await state.set_state(WordsStudy.new_word)
+    # await bot.delete_message(chat_id=callback.message.chat.id, message_id=callback.message.message_id)
+
+
+@dp.callback_query(F.data == "popular_word")
+async def send_message_beginning_study(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer(f"Great! let's start")
+    data = await state.get_data()
+    data["category"] = callback.message.text
+    print(data)
+    await state.update_data(data)
+    # res = await get_new_word(callback.message.from_user.id, 'популярные слова')
+    # en_word = res[0].get('en_word').get('word')
+    # ru_word = res[0].get('ru_word').get('word')
+    # await callback.message.answer(f"{en_word} - {ru_word}", reply_markup=get_button_keyboard())
+    # await state.set_state(WordsStudy.new_word)
+
+
 
--- bot/bot_app/keyboards.py
+++ bot/bot_app/keyboards.py
@@ -1,11 +1,23 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, \
                     ReplyKeyboardMarkup, KeyboardButton
 
+category = ["popular_word", "animals_word", "colloquial_phrases"]
+
 
 def get_menu_inline():
     buttons = [
         [InlineKeyboardButton(text="Изучать новые слова", callback_data="new_word")],
-        [InlineKeyboardButton(text="Слова выученные наполовину", callback_data="word_repetition")]
+        [InlineKeyboardButton(text="Слова выученные наполовину", callback_data="repetition_word")]
+    ]
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
+
+
+def get_category_inline():
+    buttons = [
+        [InlineKeyboardButton(text="Популярные слова", callback_data="popular_word")],
+        [InlineKeyboardButton(text="Животные", callback_data="animals_word")],
+        [InlineKeyboardButton(text="Разговорные фразы", callback_data="colloquial_phrases")]
     ]
     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
     return keyboard
@@ -17,10 +29,10 @@ def get_button_keyboard():
             KeyboardButton(text='Знаю')
             ],
         [
-            KeyboardButton(text='Не знаю')
+            KeyboardButton(text='Изучать')
             ],
         [
-            KeyboardButton(text='Закончить')
+            KeyboardButton(text='Закончить упражнение')
             ],
         ]
 
